89c62dfda80c3e2fe153a1d716fe0315e70b3300
Bee's stuff <tg999hqp2g@privaterelay.appleid.com>
2026-01-18 00:57:10 +0100

    Implement keepAlive server using Express

---
+++ keepalive.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+function keepAlive() {
+  const app = express();
+
+  app.get("/", (req, res) => {
+    res.send("Bot is alive");
+  });
+
+  app.listen(3000, () => {
+    console.log("KeepAlive server running");
+  });
+}
+
+module.exports = keepAlive;
