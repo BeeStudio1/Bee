74bc8d3b9adef2a9bccc23a0644254ab398b2afd
Bee's stuff <tg999hqp2g@privaterelay.appleid.com>
2026-01-18 12:25:17 +0000

    update keepalive and deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,29 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("hugtinounours")
-    .setDescription("Fait un câlin à Tinounours !")
-].map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync(path.join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log("Déploiement des commandes slash...");
+    try {
+        console.log("Déploiement des commandes slash (GUILD)...");
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
 
-    console.log("Commandes déployées !");
-  } catch (error) {
-    console.error(error);
-  }
+        console.log("Commandes déployées instantanément !");
+    } catch (error) {
+        console.error(error);
+    }
 })();
--- keepalive.js
+++ keepalive.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 function keepAlive() {
   const app = express();
@@ -7,9 +8,15 @@ function keepAlive() {
     res.send("Bot is alive");
   });
 
-  app.listen(3000, () => {
+  // Serveur keepAlive sur un port différent de ton API
+  app.listen(8081, () => {
     console.log("KeepAlive server running");
   });
+
+  // Auto-ping toutes les 4 minutes
+  setInterval(() => {
+    fetch("https://bee-production-72de.up.railway.app").catch(() => {});
+  }, 4 * 60 * 1000);
 }
 
 module.exports = keepAlive;
